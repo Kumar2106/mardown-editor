3886ea49d9a706e211ad388665495cb11ef7aac8
Kumar2106 <ka09934147002@gmail.com>
2021-06-01 22:19:35 +0530

    made two division

---
+++ src/Components/Markdownoutput.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+import "./Markdown.css";
+
+function Markdownoutput() {
+	return (
+		<div className="markdownoutput">
+			<h1> i am markdown output.</h1>
+		</div>
+	);
+}
+
+export default Markdownoutput;
---
+++ src/Components/Rawtext.js
@@ -0,0 +1,16 @@
+/** @format */
+
+import React from "react";
+import "./Rawtext.css";
+
+function Rawtext() {
+	return (
+		<div className="rawtext">
+			<div className="raw__input">
+				<textarea type="text" className="raw__text_input" rows="100" />
+			</div>
+		</div>
+	);
+}
+
+export default Rawtext;
